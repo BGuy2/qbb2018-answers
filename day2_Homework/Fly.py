322e67b7a195e6c5de4bdb59288fe019a9b2db16
Brian Guy <bguy2@jhu.edu>
2018-08-28 18:00:21 -0400

    Question 1 code and answer

---
+++ day2_Homework/Fly.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) >  1:
+    f = open( sys.argv[1])
+else:
+    f = sys.stdin
+
+for line in f:
+    fields = line.rstrip("\r\n").split()
+    if "DROME" in line:
+        if len(fields) < 4:
+            continue
+        print( fields[3], fields[2] )
+
+
