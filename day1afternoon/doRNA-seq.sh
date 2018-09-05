3a232d2615f73ce9c2b8c1ac76342bdec17cbbaa
Brian Guy <bguy2@jhu.edu>
2018-09-05 19:26:16 -0400

    day 1 HW

--- day1afternoon/doRNA-seq.sh
+++ day1afternoon/doRNA-seq.sh
@@ -3,10 +3,19 @@
 GENOME=../genomes/BDGP6
 ANNOTATED=../genomes/BDGP6.Ensembl.81.gtf
 
-for SAMPLE in GENOME 
+# copied SRR files into WD
+
+for SAMPLE in SRR072893 SRR072903 SRR72905 SRR072915 
 do 
+  cd ~/qbb2018-answers/day1afternoon/
   mkdir $SAMPLE
-  hisat2-build BDGP6.fa BDGP6_index
-  samtools view -b BDGP6.sam > BDGP6.bam
+  cd ~/qbb2018-answer/day1afternoon/$SAMPLE
+  fastqc ~/data/rawdata/$SAMPLE.fastq
+  hisat2 -p 8 -x ~/qbb2018-answers/genomes/BDGP6 -U ~/data/rawdata/$SAMPLE.fastq -S $SAMPLE.sam
+  samtools view -S -b $SAMPLE.sam > -o $SAMPLE.bam
+  samtools sort $SAMPLE.bam -o $SAMPLE.sorted.bam
+  samtools index $SAMPLE.sorted.bam
+  stringtie $SAMPLE.sorted.bam -p 8 -e -G ~/qbb2018-anwers/genomes/BDGP6.Ensembl.81.gtf -o $SAMPLE-quant.gtf -B
+doner
   
 done
